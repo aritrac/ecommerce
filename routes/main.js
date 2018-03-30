f69d29af0dff4925dbee432f50ec634332d6f1d6
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-30 10:26:49 +0000

    Added global search functionality

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,52 @@
 var router      = require('express').Router();
 var Product     = require('../models/product');
 
+Product.createMapping(function(err,mapping){
+    if(err){
+        console.log("Error creating mapping");
+        console.log(err);
+    }else{
+        console.log("Mapping created");
+        console.log(mapping);
+    }
+});
+
+var stream = Product.synchronize();
+var count = 0;
+
+stream.on('data',function(){
+    count++;
+});
+
+stream.on('close',function(){
+    console.log("ElasticIndex indexed " + count + " documents");
+});
+
+stream.on('error',function(err){
+    console.log(err);
+});
+
+router.post('/search', function(req,res,next){
+    res.redirect('/search?q=' + req.body.q);
+})
+
+router.get('/search', function(req,res,next){
+    if(req.query.q){
+        Product.search({
+            query_string: {query: req.query.q}
+        },function(err,results){
+            if(err)return next(err);
+            var data = results.hits.hits.map(function(hit){
+                return hit;
+            });
+            res.render('main/search-result',{
+                query: req.query.q,
+                data: data
+            });
+        });
+    }
+});
+
 router.get('/',function(req,res){
     //Shows you the cookies which your browser passes to node server
     if(typeof req.cookies['connect.sid'] !== 'undefined'){
@@ -19,7 +65,6 @@ router.get('/products/:id', function(req,res,next){
            .populate('category')
            .exec(function(err, products){
                if(err) return next(err);
-               console.log(products);
                res.render('main/category', {
                    products: products
                });
