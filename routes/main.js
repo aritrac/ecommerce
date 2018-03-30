e0e1a41ad9c1504fb5a9a3270288060f450ce554
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-30 11:06:47 +0000

    Added pagination feature

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,26 @@
 var router      = require('express').Router();
 var Product     = require('../models/product');
 
+function paginate(req,res,next){
+    var perPage = 9;
+        var page = req.params.page;
+        
+    Product.find()
+           .skip(perPage * page)
+           .limit(perPage)
+           .populate('category')
+           .exec(function(err, products,next){
+               if(err) return next(err);
+               Product.count().exec(function(err,count){
+                   if(err) return next(err);
+                   res.render('main/product-main',{
+                       products: products,
+                       pages: count / perPage
+                   });
+               });
+           });
+}
+
 Product.createMapping(function(err,mapping){
     if(err){
         console.log("Error creating mapping");
@@ -47,13 +67,21 @@ router.get('/search', function(req,res,next){
     }
 });
 
-router.get('/',function(req,res){
+router.get('/',function(req,res,next){
     //Shows you the cookies which your browser passes to node server
     if(typeof req.cookies['connect.sid'] !== 'undefined'){
         console.log(req.cookies['connect.sid']);
         console.log(req.cookies);
     }
-    res.render('main/home');
+    if(req.user){
+       paginate(req,res,next); 
+    }else{
+        res.render('main/home');   
+    }
+});
+
+router.get('/page/:page', function(req,res,next){
+    paginate(req,res,next);
 });
 
 router.get('/about',function(req,res){
