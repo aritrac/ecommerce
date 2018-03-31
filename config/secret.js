971cecdb09998a2995ce646ff1b96f7ebca7dd8d
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-31 16:41:14 +0000

    Modified callback URL

--- config/secret.js
+++ config/secret.js
@@ -5,6 +5,6 @@ module.exports = {
         clientID: process.env.FACEBOOK_ID || '208897369870470',
         clientSecret: process.env.FACEBOOK_SECRET || '24720f4cbd5b86a4ff7e59fb017eda95',
         profileFields: ['emails','displayName'],
-        callbackURL: 'https://buymyitems.herokuapp.com/auth/facebook/callback'
+        callbackURL: 'http://buymyitems.herokuapp.com/auth/facebook/callback'
     }
 }
