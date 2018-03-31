d9f65fc352115b476b259d02b409f085704f6162
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-31 16:37:15 +0000

    Added new host for redirect callback

--- config/secret.js
+++ config/secret.js
@@ -5,6 +5,6 @@ module.exports = {
         clientID: process.env.FACEBOOK_ID || '208897369870470',
         clientSecret: process.env.FACEBOOK_SECRET || '24720f4cbd5b86a4ff7e59fb017eda95',
         profileFields: ['emails','displayName'],
-        callbackURL: 'https://7cd2586a91f844f1abe82ad17bba0146.vfs.cloud9.us-east-2.amazonaws.com/auth/facebook/callback'
+        callbackURL: 'https://buymyitems.herokuapp.com/auth/facebook/callback'
     }
 }
