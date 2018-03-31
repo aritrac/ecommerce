4ab2110b73cf0d1a9e9262de4ad6b45d2d23d887
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-31 03:11:50 +0000

    Adding cart schema per user

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 var router              = require('express').Router();
 var User                = require('../models/user');
+var Cart                = require('../models/cart');
 var passport            = require('passport');
 var passportConf        = require('../config/passport');
+var async               = require('async');
 
 router.get("/login", function(req,res){
     //if(req.user) return res.redirect("/");
@@ -28,31 +30,45 @@ router.get("/signup", function(req,res,next){
 });
 
 router.post('/signup', function(req,res,next){
-    var user = new User();
-    
-    user.profile.name       = req.body.name;
-    user.email              = req.body.email;
-    user.password           = req.body.password;
-    user.username           = req.body.name;
-    user.profile.picture    = user.gravatar();
-    
-    User.findOne({email: req.body.email}, function(err, existingUser){
-        if(err) return next(err);
-        if(existingUser){
-            req.flash('errors','Account with that email address already exists');
-            return res.redirect('/signup');
-        }else{
-            user.save(function(err, user){
-                if(err) {
-                    return next(err);
+    async.waterfall([
+        function(callback){
+            var user = new User();
+            user.profile.name       = req.body.name;
+            user.email              = req.body.email;
+            user.password           = req.body.password;
+            user.username           = req.body.name;
+            user.profile.picture    = user.gravatar();
+            
+            User.findOne({email: req.body.email}, function(err, existingUser){
+                if(err) return next(err);
+                if(existingUser){
+                    req.flash('errors','Account with that email address already exists');
+                    return res.redirect('/signup');
+                }else{
+                    user.save(function(err, user){
+                        if(err) {
+                            return next(err);
+                        }
+                        callback(null,user);
+                    });
                 }
+            });
+        },
+        function(user){
+            var cart = new Cart();
+            cart.owner = user._id;
+            cart.save(function(err){
+                if(err) return next(err);
                 req.logIn(user, function(err){
-                   if(err) return next(err);
-                   res.redirect("/profile");
+                    if(err) return next(err);
+                    res.redirect('/profile');
                 });
             });
         }
-    });
+        
+    ]);
+    
+    
 });
 
 router.get('/logout', function(req,res,next){
